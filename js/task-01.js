2d9482b13543fa755bbc706b14c6307eeef22572
Ukro <koliaso@gmail.com>
2023-08-10 11:15:26 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,7 @@
- // Отримуємо елемент списку категорій за його ідентифікатором
-    const categoriesList = document.getElementById('categories');
+const allCategories = document.querySelectorAll(".item");
+console.log("Number of categories: " + allCategories.length);
 
-    // Отримуємо всі елементи li з класом 'item' у списку категорій
-    const categoryItems = categoriesList.querySelectorAll('li.item');
-    // Виводимо кількість категорій
-    console.log(`Number of categories: ${categoryItems.length}`);
-
-    // Ітеруємося по кожному елементі li з класом 'item'
-    categoryItems.forEach((category) => {
-      // Отримуємо заголовок категорії (елемент <h2>)
-      const categoryName = category.querySelector('h2').textContent;
-
-      // Отримуємо всі елементи li, що належать даній категорії
-      const categoryElements = category.querySelectorAll('li');
-
-      // Виводимо інформацію про категорію та кількість елементів в ній
-      console.log(`Category: ${categoryName}`);
-      console.log(`Elements: ${categoryElements.length}`);
-    });
+allCategories.forEach((elem) => {
+  console.log("Category: " + elem.firstElementChild.textContent);
+  console.log("Elements: " + elem.lastElementChild.children.length);
+});
