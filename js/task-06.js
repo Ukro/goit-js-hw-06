13d8a78a1c4c7251d1a0350142e4b1f9fbf83e26
Ukro <koliaso@gmail.com>
2023-08-12 10:12:02 +0300

    trim_06

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector("input");
 const lengthEl = inputEl.getAttribute("data-length");
 
 const onInputBlur = (event) =>
-  event.target.value.length === Number.parseInt(lengthEl)
-    ? (inputEl.className = "valid")
+event.target.value.trim().length === Number.parseInt(lengthEl)
+? (inputEl.className = "valid")
     : (inputEl.className = "invalid");
 inputEl.addEventListener("blur", onInputBlur);
